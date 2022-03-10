be5043332f089ecc9fb36b14e21320510cb0815e
Seunghoya <viva.primavera121@gmail.com>
2022-03-10 11:20:31 +0900

    Add Cors

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const morgan = require('morgan');
+const cors = require('cors');
 const app = express()
 const port = 4000
 
@@ -6,7 +8,16 @@ const router = require('./router/index');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'PATCH'],
+    maxAge: 3600
+  })
+);
 
 app.use('/', router);
 
